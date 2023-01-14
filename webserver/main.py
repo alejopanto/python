768141ab6797803ee054ceed7ac08ebf7526a95a
alejopanto <jose.pantoja@testu.co>
2023-01-14 15:07:49 -0500

    fastapi and uvicorn

--- webserver/main.py
+++ webserver/main.py
@@ -1,4 +1,19 @@
 import store
+from fastapi import FastAPI
+from fastapi.responses import HTMLResponse
+
+app = FastAPI()
+
+@app.get('/') #decorator
+def get_list():
+    return [1,2,3,4]
+
+@app.get('/contac', response_class=HTMLResponse) #decorator with output in html
+def get_list():
+    return """
+        <h1>Hoy soy un Titular</h1>
+        <p>y Yo soy un parrafo</p>
+    """
 
 def run():
     store.get_categories()
